0fd35c5539f16a3bbfa7f9bf606987ca1902cfff
Adam Heczko <adam.heczko@gmail.com>
2021-10-01 15:00:30 +0200

    Ideas

--- core/src/com/renderwaves/ld49/Game.java
+++ core/src/com/renderwaves/ld49/Game.java
@@ -2,24 +2,29 @@ package com.renderwaves.ld49;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Sprite sprite;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		sprite = new Sprite(img);
+
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		sprite.draw(batch);
 		batch.end();
 	}
 	
